8cc4b606fd49830c374d192dd811437d4865a692
Aesthetiful <62633768+DrChic0@users.noreply.github.com>
2024-11-05 11:29:34 -0800

    Delete modules directory

--- modules/footer.php
+++
@@ -1,4 +0,0 @@
-<br><nav class="navbar navbar-expand-lg navbar-light text-white  text-center footer">
-                <span>Novetus Server List © 2024</span>
-                <span class="justify-content-center" style="width: 50%;margin-left: auto;margin-right: auto;">novetusserverlist.com is an independent platform and is not affiliated or associated with the Novetus project or ROBLOX Corporation.</span>
-            </nav>
